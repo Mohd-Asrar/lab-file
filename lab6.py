06be7d80c35d17fda302044998d5da3eeb3b895d
Mohd Asrar <61001575+mohdasrarmru@users.noreply.github.com>
2020-02-28 14:00:25 +0530

    Add files via upload

--- lab6.py
+++ lab6.py
@@ -33,6 +33,74 @@ for d in (dic1, dic2, dic3): dic4.update(d)
 print(dic4)
 
 
+# In[12]:
+
+
+#Intersection
+setx = set(["green", "blue"])
+sety = set(["blue", "yellow"])
+setz = setx & sety
+print(setz)
+
+
+# In[13]:
+
+
+#Union
+setx = set(["green", "blue"])
+sety = set(["blue", "yellow"])
+seta = setx | sety
+print(seta)
+
+
+# In[14]:
+
+
+setp = set(["Red", "Green"])
+setq = setp.copy()
+print(setq)
+setq.clear()
+print(setq)
+
+
+# In[15]:
+
+
+setx = set(["apple", "mango"])
+sety = set(["mango", "orange"])
+setz = set(["mango"])
+issubset = setx <= sety
+print(issubset)
+issuperset = setx >= sety
+print(issuperset)
+issubset = setz <= sety
+print(issubset)
+issuperset = sety >= setz
+print(issuperset)
+
+
+# In[16]:
+
+
+setx = set(["apple", "mango"])
+sety = set(["mango", "orange"])
+setz = setx & sety
+print(setz)
+#Set difference
+setb = setx - setz
+print(setb)
+
+
+# In[17]:
+
+
+setx = set(["apple", "mango"])
+sety = set(["mango", "orange"])
+#Symmetric difference
+setc = setx ^ sety
+print(setc)
+
+
 # In[ ]:
 
 
