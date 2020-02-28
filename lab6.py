d880e539a34cece02f2538a3cc033e3628474b10
Mohd Asrar <61001575+mohdasrarmru@users.noreply.github.com>
2020-02-28 13:14:31 +0530

    Add files via upload

---
+++ lab6.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+dic4={}
+for d in (dic1, dic2, dic3): dic4.update(d)
+print(dic4)
+
+
+# In[10]:
+
+
+d=dict()
+for x in range(1,16):
+    d[x]=x**2
+print(d)  
+ 
+
+
+# In[11]:
+
+
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+dic4={}
+for d in (dic1, dic2, dic3): dic4.update(d)
+print(dic4)
+
+
+# In[ ]:
+
+
+
+
